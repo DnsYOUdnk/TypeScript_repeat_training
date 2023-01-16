84b969e9997cf4dd49f320aa469ee995cd9a341c
Denis Yudenko <dns.yudenka@gmail.com>
2023-01-17 02:45:01 +0300

    fix: module03

--- code/module-03/m03-start/build/module03.js
+++ code/module-03/m03-start/build/module03.js
@@ -4,22 +4,45 @@
 /*  TODO: Update the calculateInterestOnlyLoanPayment function. */
 function calculateInterestOnlyLoanPayment(loanTerms) {
     // Calculates the monthly payment of an interest only loan
-    const { interestRate, principle } = loanTerms;
+    const { interestRate, principal } = loanTerms;
     let interest = interestRate / 1200; // Calculates the Monthly Interest Rate of the loan
     let payment;
-    payment = principle * interest;
+    payment = principal * interest;
     return 'The interest only loan payment is ' + payment.toFixed(2);
 }
 /*  TODO: Update the calculateConventionalLoanPayment function. */
 function calculateConventionalLoanPayment(convetionalLoanTerms) {
     // Calculates the monthly payment of a conventional loan
-    const { interestRate, principle, months } = convetionalLoanTerms;
+    const { interestRate, principal, months } = convetionalLoanTerms;
     let interest = interestRate / 1200; // Calculates the Monthly Interest Rate of the loan
     let payment;
-    payment = principle * interest / (1 - (Math.pow(1 / (1 + interest), months)));
+    payment = principal * interest / (1 - (Math.pow(1 / (1 + interest), months)));
     return 'The conventional loan payment is ' + payment.toFixed(2);
 }
-let interestOnlyPayment = calculateInterestOnlyLoanPayment({ interestRate: 30000, principle: 5 });
-let conventionalPayment = calculateConventionalLoanPayment({ interestRate: 30000, principle: 5, months: 180 });
+let interestOnlyPayment = calculateInterestOnlyLoanPayment({ principal: 30000, interestRate: 5 });
+let conventionalPayment = calculateConventionalLoanPayment({ principal: 30000, interestRate: 5, months: 180 });
 console.log(interestOnlyPayment); //* Returns "The interest only loan payment is 125.00" 
 console.log(conventionalPayment); //* Returns "The conventional loan payment is 237.24" 
+console.log('----------------------------------------------------------------------------')
+
+async function fn() {
+    let start = Date.now();
+    
+    console.log('Starting operation');
+    
+    let onePromise = await fetch('https://httpbin.org/delay/3');
+    let twoPromise = await fetch('https://httpbin.org/delay/2');
+    
+    console.log('Other work');
+    console.log('Please wait ... ');
+    
+    const oneResponse = await onePromise;
+    const twoResponse = await twoPromise;
+    
+    const finishTime = Date.now() - start;
+
+    console.log(`Operation took ${finishTime} seconds`);
+    
+}
+
+fn();
